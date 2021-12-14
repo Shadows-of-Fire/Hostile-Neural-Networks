d490948e843bba8a1bdd9c26903b5d6dea335ae0
Brennan Ward <Bward7864@gmail.com>
2021-12-13 22:01:39 -0500

    bump ver

--- src/main/java/shadows/hostilenetworks/HostileNetworks.java
+++ src/main/java/shadows/hostilenetworks/HostileNetworks.java
@@ -44,7 +44,7 @@ import shadows.placebo.network.MessageHelper;
 public class HostileNetworks {
 
 	public static final String MODID = "hostilenetworks";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "2.0.1";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	//Formatter::off
     public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder
