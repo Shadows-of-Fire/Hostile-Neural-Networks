3c475ade46b901d61c510aa2d88f6e2022efcc3e
Brennan Ward <Bward7864@gmail.com>
2021-12-14 20:41:00 -0500

    bump ver

--- changelog.md
+++ changelog.md
@@ -1,3 +1,6 @@
+## 3.0.2
+* Actually apply the first change from 1.0.2
+
 ## 3.0.1
 * Same changes as 1.0.2
 
--- src/main/java/shadows/hostilenetworks/HostileNetworks.java
+++ src/main/java/shadows/hostilenetworks/HostileNetworks.java
@@ -46,7 +46,7 @@ import shadows.placebo.network.MessageHelper;
 public class HostileNetworks {
 
 	public static final String MODID = "hostilenetworks";
-	public static final String VERSION = "2.0.1";
+	public static final String VERSION = "3.0.2";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	//Formatter::off
     public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder
