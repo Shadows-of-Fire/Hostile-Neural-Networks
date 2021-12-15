75e42ec2b854adcd6eedbd10c138cd569c67e6f5
Brennan Ward <Bward7864@gmail.com>
2021-12-14 20:12:53 -0500

    3.0.1

--- src/main/java/shadows/hostilenetworks/block/LootFabBlock.java
+++ src/main/java/shadows/hostilenetworks/block/LootFabBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.MenuProvider;
@@ -17,6 +18,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.items.wrapper.RecipeWrapper;
 import net.minecraftforge.network.NetworkHooks;
 import shadows.hostilenetworks.gui.LootFabContainer;
 import shadows.hostilenetworks.tile.LootFabTileEntity;
@@ -53,6 +55,19 @@ public class LootFabBlock extends HorizontalDirectionalBlock implements TickingE
 			return InteractionResult.CONSUME;
 		}
 	}
+	
+	@Override
+	@Deprecated
+	public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+		if (!pState.is(pNewState.getBlock())) {
+			BlockEntity te = pLevel.getBlockEntity(pPos);
+			if (te instanceof LootFabTileEntity fab) {
+				Containers.dropContents(pLevel, pPos, new RecipeWrapper(fab.getInventory()));
+			}
+			super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+		}
+	}
+
 
 	@Override
 	public MenuProvider getMenuProvider(BlockState pState, Level pLevel, BlockPos pPos) {
--- src/main/java/shadows/hostilenetworks/block/SimChamberBlock.java
+++ src/main/java/shadows/hostilenetworks/block/SimChamberBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.MenuProvider;
@@ -17,6 +18,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.items.wrapper.RecipeWrapper;
 import net.minecraftforge.network.NetworkHooks;
 import shadows.hostilenetworks.gui.SimChamberContainer;
 import shadows.hostilenetworks.tile.SimChamberTileEntity;
@@ -52,6 +54,18 @@ public class SimChamberBlock extends HorizontalDirectionalBlock implements Ticki
 	@Override
 	public MenuProvider getMenuProvider(BlockState pState, Level pLevel, BlockPos pPos) {
 		return new SimpleMenuProvider((id, inv, player) -> new SimChamberContainer(id, inv, pPos), new TranslatableComponent(this.getDescriptionId()));
+	}
+	
+	@Override
+	@Deprecated
+	public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+		if (!pState.is(pNewState.getBlock())) {
+			BlockEntity te = pLevel.getBlockEntity(pPos);
+			if (te instanceof SimChamberTileEntity sim) {
+				Containers.dropContents(pLevel, pPos, new RecipeWrapper(sim.getInventory()));
+			}
+			super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+		}
 	}
 
 	@Override
