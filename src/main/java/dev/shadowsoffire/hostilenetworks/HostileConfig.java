99eca6dc3d2c061857c585511efe195ea043b0c6
Max <maxh2709@gmail.com>
2024-03-09 02:03:41 +0100

    Remove unnecessary import

--- src/main/java/dev/shadowsoffire/hostilenetworks/HostileConfig.java
+++ src/main/java/dev/shadowsoffire/hostilenetworks/HostileConfig.java
@@ -1,6 +1,5 @@
 package dev.shadowsoffire.hostilenetworks;
 
-import dev.shadowsoffire.hostilenetworks.data.ModelTier;
 import dev.shadowsoffire.placebo.config.Configuration;
 
 public class HostileConfig {
