06f113f988eed14cd369b1e3abe0be543c5d5a07
Max <maxh2709@gmail.com>
2024-03-09 02:42:50 +0100

    Add config option to limit upgrade capabilities of the simulation chamber

--- src/main/java/dev/shadowsoffire/hostilenetworks/HostileConfig.java
+++ src/main/java/dev/shadowsoffire/hostilenetworks/HostileConfig.java
@@ -8,6 +8,7 @@ public class HostileConfig {
     public static int fabPowerCap = 1000000;
     public static int fabPowerCost = 256;
     public static boolean continuousAccuracy = false;
+    public static int simModelUpgrade = 0;
 
     public static void load() {
         Configuration cfg = new Configuration(HostileNetworks.MODID);
@@ -15,6 +16,7 @@ public class HostileConfig {
         fabPowerCap = cfg.getInt("Loot Fab Power Cap", "power", fabPowerCap, 1, Integer.MAX_VALUE, "The maximum FE stored in the Loot Fabricator.");
         fabPowerCost = cfg.getInt("Loot Fab Power Cost", "power", fabPowerCost, 0, Integer.MAX_VALUE, "The FE/t cost of the Loot Fabricator.");
         continuousAccuracy = cfg.getBoolean("Continuous Accuracy", "models", continuousAccuracy, "If true, the accuracy of the model increases as it gains progress towards the next tier. If false, always uses the base accuracy of the current tier.");
+        simModelUpgrade = cfg.getInt("Sim Chamber Upgrades Model", "models", simModelUpgrade, 0,2, "Whether the Simulation Chamber will upgrade the data on a model. (0 = No, 1 = Yes, 2 = Only up to tier boundaries)");
         if (cfg.hasChanged()) cfg.save();
     }
 
