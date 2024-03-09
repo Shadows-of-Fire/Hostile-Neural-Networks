ce4b437b1018b73f904cc048b388aaf81e0c5134
Max <maxh2709@gmail.com>
2024-03-09 02:27:41 +0100

    Change tier of config option to something more appropriate

--- src/main/java/dev/shadowsoffire/hostilenetworks/HostileConfig.java
+++ src/main/java/dev/shadowsoffire/hostilenetworks/HostileConfig.java
@@ -14,7 +14,7 @@ public class HostileConfig {
         simPowerCap = cfg.getInt("Sim Chamber Power Cap", "power", simPowerCap, 1, Integer.MAX_VALUE, "The maximum FE stored in the Simulation Chamber.");
         fabPowerCap = cfg.getInt("Loot Fab Power Cap", "power", fabPowerCap, 1, Integer.MAX_VALUE, "The maximum FE stored in the Loot Fabricator.");
         fabPowerCost = cfg.getInt("Loot Fab Power Cost", "power", fabPowerCost, 0, Integer.MAX_VALUE, "The FE/t cost of the Loot Fabricator.");
-        continuousAccuracy = cfg.getBoolean("Continuous Accuracy", "accuracy", continuousAccuracy, "If true, the accuracy of the model increases as it gains progress towards the next tier. If false, always uses the base accuracy of the current tier.");
+        continuousAccuracy = cfg.getBoolean("Continuous Accuracy", "models", continuousAccuracy, "If true, the accuracy of the model increases as it gains progress towards the next tier. If false, always uses the base accuracy of the current tier.");
         if (cfg.hasChanged()) cfg.save();
     }
 
