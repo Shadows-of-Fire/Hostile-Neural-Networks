fe40b587d9ead3619770b2b1d0a8efbe490caf9b
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-10-22 12:10:33 -0700

    Re-add Curios logic
    Closes #61

--- src/main/java/dev/shadowsoffire/hostilenetworks/curios/CuriosCompat.java
+++ src/main/java/dev/shadowsoffire/hostilenetworks/curios/CuriosCompat.java
@@ -1,11 +1,14 @@
 package dev.shadowsoffire.hostilenetworks.curios;
 
+import dev.shadowsoffire.hostilenetworks.Hostile;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotResult;
 
 public class CuriosCompat {
 
     public static ItemStack getDeepLearner(Player player) {
-        return ItemStack.EMPTY;// return CuriosApi.getCuriosHelper().findFirstCurio(player, Hostile.Items.DEEP_LEARNER.get()).map(SlotResult::stack).orElse(ItemStack.EMPTY);
+        return CuriosApi.getCuriosInventory(player).map(inv -> inv.findFirstCurio(Hostile.Items.DEEP_LEARNER.value()).map(SlotResult::stack).orElse(ItemStack.EMPTY)).orElse(ItemStack.EMPTY);
     }
 }
