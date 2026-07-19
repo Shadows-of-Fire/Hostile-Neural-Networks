b62f5b6946ecfd3f6cf5ccf412eaa695894f7582
Shadows_of_Fire <Bward7864@gmail.com>
2026-07-19 04:11:38 -0400

    Add some modded glass blocks to valid walls

--- src/main/java/dev/shadowsoffire/hostilenetworks/datagen/BlockTagProvider.java
+++ src/main/java/dev/shadowsoffire/hostilenetworks/datagen/BlockTagProvider.java
@@ -6,6 +6,7 @@ import dev.shadowsoffire.hostilenetworks.Hostile;
 import dev.shadowsoffire.hostilenetworks.HostileNetworks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
@@ -20,6 +21,14 @@ public class BlockTagProvider extends BlockTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider provider) {
         this.tag(Hostile.Tags.DATA_CENTER_FLOOR).addTag(Tags.Blocks.OBSIDIANS);
-        this.tag(Hostile.Tags.DATA_CENTER_WALL).add(Blocks.BLACK_STAINED_GLASS).add(Hostile.Blocks.DATA_CENTER_IO_PORT.value());
+        this.tag(Hostile.Tags.DATA_CENTER_WALL)
+            .add(Blocks.BLACK_STAINED_GLASS)
+            .add(Hostile.Blocks.DATA_CENTER_IO_PORT.value())
+            .addOptionalTag(ResourceLocation.fromNamespaceAndPath("chipped", "black_stained_glass"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath("connectedglass", "borderless_glass_black"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath("connectedglass", "scratched_glass_black"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath("connectedglass", "clear_glass_black"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath("connectedglass", "tinted_borderless_glass_black"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath("securitycraft", "reinforced_black_stained_glass"));
     }
 }
